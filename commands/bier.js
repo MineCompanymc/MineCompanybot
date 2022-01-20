920518fe6fabd4c1661a84782ff8f8a5a6009515
SimbaBasil <simbabasilmc@gmail.com>
2022-01-20 16:25:12 +0100

    Update bier.js

--- commands/bier.js
+++ commands/bier.js
@@ -1,9 +1,20 @@
 const discord = require("discord.js");
+const engels = ['933356489760972867'];
+const nederlands = ['933354936551473232'];
 
 module.exports.run = async (bot, message, args) => {
 
-    message.channel.send(`hier neem dit: <:stella:891404782353408070>`);
-    message.react("<:stella:891404782353408070>");
+    if (engels.some(role => message.member.roles.cache.get(role))){
+
+        message.channel.send(`Here take this: <:stella:891404782353408070>`);
+        message.react("<:stella:891404782353408070>");
+
+    }else if (nederlands.some(role => message.member.roles.cache.get(role))){
+
+        message.channel.send(`Hier neem dit: <:stella:891404782353408070>`);
+        message.react("<:stella:891404782353408070>");
+
+    }else return message.react("jij moet nog een rol claimen/you still have to claim a role");
 
 }
 
