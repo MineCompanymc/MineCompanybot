5c0665787262d94fb465f84a6b26b2893e5d6a20
SimbaBasil <simbabasilmc@gmail.com>
2022-01-20 15:35:25 +0100

    Update gm.js

--- commands/gm.js
+++ commands/gm.js
@@ -9,7 +9,7 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.info = {
     name: "gm",
-    aliases: ["GM", "Gm", "gM",],
+    aliases: ["GM","Gm","gM",],
     description: "goede morgen!",
     category: "Spelletjes"
 }
